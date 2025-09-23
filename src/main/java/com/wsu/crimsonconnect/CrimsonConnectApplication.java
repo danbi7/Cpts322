4ac1aa3932ea74b7cf8995eeb2bced7d62072bd8
Danbi <danbik77@gmail.com>
2025-09-23 13:14:52 -0700

    Add frontend folder without submodule

---
+++ src/main/java/com/wsu/crimsonconnect/CrimsonConnectApplication.java
@@ -0,0 +1,13 @@
+package com.wsu.crimsonconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrimsonConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrimsonConnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wsu/crimsonconnect/CrimsonConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wsu.crimsonconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrimsonConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
