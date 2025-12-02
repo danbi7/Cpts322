d649068b11cc712fe23a8f7fd4add63172fd851c
kenson05 <kenneth.son@wsu.edu>
2025-12-02 15:41:59 -0800

    changed api url

--- src/frontend/src/services/api.ts
+++ src/frontend/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://crimson-connect-production-9cee.up.railway.app';
+const API_BASE_URL = 'https://crimson-connect-production-9cee.up.railway.app/api';
 
 const api = axios.create({
     baseURL: API_BASE_URL,
